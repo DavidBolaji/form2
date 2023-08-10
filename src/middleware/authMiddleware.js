908189a5fab3f2050170ddb88d60b0696d7f454f
davidbolaji <odavidbolaji14@gmail.com>
2023-08-10 15:12:13 +0100

    fixed sign out logic

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -13,8 +13,10 @@ const auth = (req, res, next) => {
         return res.status(401).send({ message: "Unauthorized" });
       }
       if (
-        result.email !== process.env.EMAIL ||
-        result.email !== process.env.EMAIL2
+        !(
+          result.email !== process.env.EMAIL ||
+          result.email !== process.env.EMAIL2
+        )
       ) {
         return res.status(401).send({ message: "Unauthorized" });
       }
